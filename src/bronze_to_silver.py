43da54d70ab9cb80dd24635927a6a00f95c90124
Luc4sD3v <lucasrafaelc3po@gmail.com>
2025-12-07 01:53:31 -0300

    Enhance comments for clarity in bronze_to_silver.py
    Added comments to clarify the process and limit in the code.

--- src/bronze_to_silver.py
+++ src/bronze_to_silver.py
@@ -2,23 +2,33 @@ import duckdb
 from pathlib import Path
 
 def run_bronze_to_silver(cfg=None):
+    # Informa o início do processo
     print("🚀 Iniciando Bronze → Silver")
 
+    # Caminho do arquivo CSV na camada Bronze
     bronze_path = Path("data/bronze/SAML-D.csv")
+
+    # Diretório de saída da camada Silver
     silver_path = Path("data/silver")
+
+    # Garante que o diretório exista (cria se não existir)
     silver_path.mkdir(parents=True, exist_ok=True)
 
+    # Conexão com o banco DuckDB armazenado na camada Silver
     con = duckdb.connect(database="data/silver/silver.duckdb")
 
+    # Carrega o CSV para uma tabela RAW no DuckDB
     con.execute(f"""
         CREATE OR REPLACE TABLE raw AS
         SELECT *
         FROM read_csv_auto('{bronze_path}')
-        LIMIT 1000000
+        LIMIT 1000000  -- Limita a 1 milhão de linhas (se desejar remover, apagar esta linha)
     """)
 
+    # Confirma que o CSV foi carregado
     print("✅ CSV carregado no DuckDB.")
 
+    #padronizada e deixa pronto para análises
     con.execute("""
         CREATE OR REPLACE TABLE silver_transactions AS
         SELECT
@@ -42,6 +52,7 @@ def run_bronze_to_silver(cfg=None):
 
     print("✅ Transformações aplicadas.")
 
+    # Exporta a tabela final para um arquivo Parquet
     con.execute("""
         COPY silver_transactions
         TO 'data/silver/silver_transactions.parquet'
@@ -50,7 +61,9 @@ def run_bronze_to_silver(cfg=None):
 
     print("✅ Camada SILVER gerada com sucesso.")
 
+    # Fecha conexão com o banco DuckDB
     con.close()
 
+# Executa a função se o arquivo for rodado diretamente
 if __name__ == "__main__":
     run_bronze_to_silver()
